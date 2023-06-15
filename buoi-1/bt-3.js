501930607f2dcce8cd031819eee044410357044f
vubao2001 <levubao2001@gmail.com>
2023-06-16 01:29:14 +0700

    bai tap 3

--- buoi-1/bt-3.js
+++ buoi-1/bt-3.js
@@ -1,6 +1,23 @@
-function obj (arr) {
-    let newArray = [];
-    let cout = 0;
 
-    
+function checkCount(arr) {
+    const countDict = {};
+    let maxCount = 0;
+    let result = null;
+
+    for (let i = 0; i < arr.length; i++) {
+        const key = arr[i];
+        if (countDict[key]) {
+            countDict[key]++;
+        } else {
+            countDict[key] = 1;
+        }
+        if (countDict[key] > maxCount) {
+            maxCount = countDict[key];
+            result = key;
+        }
+    }
+
+    return { value: result, count: maxCount };
 }
+
+console.log(checkCount([1, 2, 3, 5, 5, 6, 6]))
